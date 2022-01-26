880a53c6e499d1bfdae0096ab4f14724f0b15113
N1crom <98448300+N1crom@users.noreply.github.com>
2022-01-26 16:08:25 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -350,11 +350,11 @@
             return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
-        return '人？';
+        if (score <= 49) return 'Novice';
+        if (score <= 99) return 'Regular';
+        if (score <= 149) return 'Veteran';
+        if (score <= 199) return 'Legend';
+        return '???';
     }
 
     w.toStr = function(obj) {
